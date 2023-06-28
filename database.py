58b8d149d312a37d0a0045989512dd03df84e88c
Your Name <you@example.com>
2023-06-28 15:12:07 +0800

    init

---
+++ database.py
@@ -0,0 +1,69 @@
+import mysql.connector
+import os
+
+class Database:
+    def __init__(self):
+
+        self.host = os.getenv('DB_HOST')
+        self.port = os.getenv('DB_PORT')
+        self.user = os.getenv('DB_USER')
+        self.password = os.getenv('DB_PASSWORD')
+        self.database = os.getenv('DB_NAME')
+
+        self.cnx = mysql.connector.connect(
+            host=self.host,
+            port=self.port,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+
+        self.cursor = self.cnx.cursor()
+
+        create_table_query = """
+            CREATE TABLE guardians (
+              address VARCHAR(255) NOT NULL,
+              info VARCHAR(255) NOT NULL,
+              type VARCHAR(50) NOT NULL,
+              PRIMARY KEY (address, info)
+            );
+        """
+
+        try:
+            self.cursor.execute(create_table_query)
+        except mysql.connector.Error as error:
+            print("Error creating table:", error)
+
+    def __del__(self):
+        self.cursor.close()
+        self.cnx.close()
+
+    def add_guardian(self, address, guardian_type, guardian_info):
+        insert_query = "INSERT INTO guardians (address, type, info) VALUES (%s, %s, %s)"
+        insert_data = (address, guardian_type, guardian_info)
+        self.cursor.execute(insert_query, insert_data)
+        self.cnx.commit()
+
+    def get_guardians_by_address(self, address):
+        query = "SELECT * FROM guardians WHERE address = %s"
+        self.cursor.execute(query, (address,))
+        results = self.cursor.fetchall()
+
+        guardians = []
+        for row in results:
+            guardian = {
+                'type': row[1],
+                'info': row[2]
+            }
+            guardians.append(guardian)
+
+        return guardians
+
+    def get_addresses_by_guardian(self, guardian_type):
+        query = "SELECT address FROM guardians WHERE type = %s"
+        self.cursor.execute(query, (guardian_type,))
+        results = self.cursor.fetchall()
+
+        addresses = [row[0] for row in results]
+
+        return addresses
---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from database import Database
+
+app = Flask(__name__)
+
+db = Database()
+
+@app.route('/guardians', methods=['POST'])
+def add_guardian():
+    data = request.get_json()
+    address = data['address']
+    guardian_type = data['type']
+    guardian_info = data['info']
+
+    db.add_guardian(address, guardian_type, guardian_info)
+
+    return jsonify({'message': 'Guardian added successfully.'})
+
+@app.route('/guardians/address/<address>', methods=['GET'])
+def get_guardians_by_address(address):
+    guardians = db.get_guardians_by_address(address)
+
+    return jsonify({'guardians': guardians})
+
+@app.route('/addresses/guardian', methods=['GET'])
+def get_addresses_by_guardian():
+    guardian_type = request.args.get('type')
+    addresses = db.get_addresses_by_guardian(guardian_type)
+
+    return jsonify({'addresses': addresses})
+
+if __name__ == '__main__':
+    app.run()
